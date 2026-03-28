ababce0f8a05b3986831869bdf38990e2d15195c
Yegor Kuznetsov <yegor@tydbits.com>
2026-03-28 01:13:34 -0700

    initial setup and added fish config

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+# Dotflies
+
+Personal dotfiles.
+
+I made a typo when making the directory and decided to keep it because I find it more fun than calling the repo dotfiles.
+
---
+++ download_dotter.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+REPO="SuperCuber/dotter"
+ASSET="dotter-linux-x64-musl"
+
+# Alternative to download latest instead...
+#LATEST_TAG=$(curl --silent "https://api.github.com/repos/$REPO/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+#wget "https://github.com/$REPO/releases/download/$LATEST_TAG/$ASSET"
+
+# Instead let's just pin the version to v0.13.4 and manually update in the future.
+TAG="v0.13.4"
+wget "https://github.com/$REPO/releases/download/$TAG/$ASSET" -O dotter
+chmod +x dotter
+
